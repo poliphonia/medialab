895f87022425357758996fc1c8f1ebf623f6df9e
felipefsalazar <felipefsalazar@gmail.com>
2018-06-21 18:12:59 -0300

    código do arduino / processing
    processing o código é do Thiago

---
+++ Makeathon/Rat-o Responsivo/HCSR04.ino
@@ -0,0 +1,24 @@
+#include <HCSR04.h>
+
+HCSR04 hc(5, 6); //initialisation class HCSR04 (trig pin , echo pin)
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+//  if ( hc.dist() > 100 )
+//    {
+//      Serial.println( "Mais que 100" );
+//    }
+//  else
+//  {
+//    Serial.println( "Alooo");
+//}
+int numero = hc.dist();
+   Serial.println(numero );  //return curent distance in serial
+   delay (100);
+}
+
